17e3b96b5ed9e92a83607c502e399e9c597a0ea2
GM <szemetes666@gmail.com>
2011-01-05 21:25:14 +0100

    Removed default Rails page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 FirstApp::Application.routes.draw do
-  get "pages/home"
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
 
-  get "pages/contact"
+  root :to => 'pages#home'
 
-  get "pages/about"
 
   resources :microposts
 
